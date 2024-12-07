2fd60f7298df7e33ba47e6101c7fe1eed35fe910
MihaiStefanoaia <mihaistefanoaia1102@gmail.com>
2024-12-07 14:25:21 +0200

    sedintele vechi

---
+++ 2024_11_23/exemplu.py
@@ -0,0 +1,16 @@
+
+# x = int(input())
+# for i in range(x):
+    # print("executam instructiune 1")
+    # print("executam instructiune 2")
+    # print("executam instructiune 3")
+    # print(i)
+
+print("executam instructiune 1")
+print("executam instructiune 2")
+print("executam instructiune 3")
+# print(i)
+
+latime = int(input())
+perimetru = 4 * latime + 20
+print(perimetru)
